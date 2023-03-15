a9cc636b3154594efc95962e4d9b27001f9c6364
ProjectZoo <100379860+ProjectZoo@users.noreply.github.com>
2023-03-15 16:31:58 -0300

    Add files via upload

---
+++ Desafio/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args){
+        Console.Write("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Seu nome é: " + nome);
+        Console.Write("Digite um número: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi " + numero1);
+        Console.Write("Digite um número: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi " + numero2);
+        
+        double media = (numero1+numero2)/2;
+        
+        Console.Write("A média é "+ media);
+    } 
+    }
