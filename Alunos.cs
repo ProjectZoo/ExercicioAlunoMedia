1eeffd5acbca277910a3bd984559428d42d326bc
ProjectZoo <100379860+ProjectZoo@users.noreply.github.com>
2023-03-15 14:45:06 -0300

    Add files via upload

---
+++ Alunos.cs
@@ -0,0 +1,28 @@
+namespace ExercicioAlunos;
+class Alunos
+{
+    public string nome="";
+    public double nota1, nota2;
+
+    //Media
+    public double obterMedia(){
+        double media = (nota1+nota2)/2;
+        return media;
+    }
+    //Situação
+    public string obterSituacao(double media){
+        string situacao="";
+        if(media>=6){
+            situacao="Aprovado";
+        }else{
+            situacao="Reprovado";
+        }
+        return situacao;
+    }
+    //Mensagem
+    public void mensagem(){
+        double mediaCalculada = obterMedia();
+        string resultadoSituacao = obterSituacao(mediaCalculada);
+        Console.WriteLine(nome+" está "+resultadoSituacao+" com média: "+mediaCalculada);
+    }
+}
